104fc99d45479c4eb3c84a0578ef798f77e11ec7
freddylauhk <freddy.kmlau@gmail.com>
2023-02-14 17:30:59 -0500

    Optimize player class

--- src/Player.py
+++ src/Player.py
@@ -1,28 +1,22 @@
 class Player:
-
-    __pieceType = 0
-    __rowCursor = 0
-    __columnCursor = 0
-
-    def __init__(self, whiteside):
+    def __init__(self, whiteside: bool) -> None:
         if whiteside:
-            self.__pieceType = 1
-            self.__rowCursor = 3
-            self.__columnCursor = 3
+            self.piece_type = 1
+            self.cursor_position = (3, 3)
             print("White side player initialized")
         else:
-            self.__pieceType = 2
-            self.__rowCursor = 3
-            self.__columnCursor = 4
+            self.piece_type = 2
+            self.cursor_position = (3, 4)
             print("Black side player initialized")
 
-    def getPieceType(self):
-        return self.__pieceType
+    def get_piece_type(self) -> int:
+        return self.piece_type
 
-    def getCursorPosition(self):
-        print("White" if self.__pieceType == 1 else "Black" , "Position is [", self.__rowCursor, ",", self.__columnCursor, "]")
-        return [self.__rowCursor, self.__columnCursor]
-    
-    def setCursorPosition(self, row, column):
-        self.__rowCursor = row
-        self.__columnCursor = column
+    def get_cursor_position(self) -> tuple[int, int]:
+        color = "White" if self.piece_type == 1 else "Black"
+        row, col = self.cursor_position
+        print(f"{color} Position is [{row}, {col}]")
+        return self.cursor_position
+
+    def set_cursor_position(self, row: int, col: int) -> None:
+        self.cursor_position = (row, col)
